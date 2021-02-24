9ebf64220c7210062ece978fd713de16e6cc4143
Faran42 <faranorphys@gmail.com>
2021-02-23 22:20:22 -0300

    Installing sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
+import 'reflect-metadata'
 import express from 'express'
+import "./database"
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Hello World - NLW04') 
-})
-
-app.post('/', (req, res) => {
-  return res.json({ message: "Os dados foram salvos com sucesso" }) 
-})
+app.use(router)
 
 app.listen(3333, () => console.log('Server is running!'));
